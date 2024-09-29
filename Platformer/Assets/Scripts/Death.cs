dbcc23b40bd69e5ef5dec2438077f886bb8ba6d0
barcysaivaras1 <BarcysA@gmail.com>
2024-09-29 13:43:04 +0100

    Added Scene Level01
    + Added Level01(Scene)
    + Added TrainObjects (Placeholders)
    + Added Ground Object (Causes Death)
    + Added Debug Text that is updated when you die

---
+++ Platformer/Assets/Scripts/Death.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using TMPro;
+
+public class Death : MonoBehaviour
+{
+    [Header("Mandatory Values")]
+    private Rigidbody rb;
+    public float playerHeight;
+    public LayerMask whatIsDanger;
+
+    [Header("DEBUG")]
+    public TMP_Text debugDeadState;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    void FixedUpdate()
+    {
+        //When you touch the danger ground, you die
+        if(Physics.Raycast(rb.position, Vector3.down, playerHeight * 0.5f + 0.1f, whatIsDanger))
+        {
+            debugDeadState.text = "Dead";
+        }
+    }
+}
