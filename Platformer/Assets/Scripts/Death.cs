7d537ab88f43b909f386ccaf93e5dfe4171c195f
zedchuu <adam.zikri.b.a@gmail.com>
2024-10-21 00:39:23 +0800

    Revert "Change build order and death script"
    This reverts commit 239a5acde9d1fd40ab67d0492ec8a1717409e028.

--- Platformer/Assets/Scripts/Death.cs
+++ Platformer/Assets/Scripts/Death.cs
@@ -1,7 +1,5 @@
 using UnityEngine;
 using TMPro;
-using UnityEngine.SceneManagement;
-
 
 public class Death : MonoBehaviour
 {
@@ -23,7 +21,6 @@ public class Death : MonoBehaviour
         if(Physics.Raycast(rb.position, Vector3.down, playerHeight * 0.5f + 0.1f, whatIsDanger))
         {
             debugDeadState.text = "Dead";
-            SceneManager.LoadScene("Menu");
         }
     }
 }
