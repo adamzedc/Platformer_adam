55f2004e51795019850c05beecd5d93eb741751b
barcysaivaras1 <BarcysA@gmail.com>
2024-10-22 00:38:36 +0100

    Water bug Fix
    + Fixed Bug where if the player entered two water objects and exited one , they would be treated as if they are no longer in water
    
    + Fixed a bug where when the player is in water and the secret triggered causing the water to disappear, the player would still be treated as if they were in water
    
    These have now been fixed :)

--- Platformer/Assets/Scripts/Water.cs
+++ Platformer/Assets/Scripts/Water.cs
@@ -8,7 +8,6 @@ public class Water : MonoBehaviour
     public LayerMask whatIsWater;
     private Rigidbody rb;
     private PlayerMovement pm;
-    private Sliding sl;
 
     [Header("Movement")]
     public float waterGroundDrag;
@@ -17,87 +16,101 @@ public class Water : MonoBehaviour
     public float waterGravity;
     public float waterDownwardForce;
     private float defaultGravity;
-    //public float waterWalkSpeed;
-    //public float waterSprintSpeed;
-    
 
     private float airDrag;
     private float groundDrag;
-    //private float walkSpeed;
-    //private float sprintSpeed;
 
-
-
-
-    // Start is called before the first frame update
-
-    // Update is called once per frame
+    private int waterCounter = 0;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
         pm = GetComponent<PlayerMovement>();
-        sl = GetComponent<Sliding>();
 
 
         defaultGravity = -9.81f;
         airDrag = pm.airDrag;
         groundDrag = pm.groundDrag;
-        //walkSpeed = pm.walkSpeed;
-        //sprintSpeed = pm.sprintSpeed;
+        waterCounter = 0;
     }
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "water")
         {
-            Vector3 dragVel = transform.InverseTransformDirection(rb.velocity);
-            //Applying drag to y axis, only downward drag
-            //Applying downwards drag the moment the player enters the water
-            if (rb.velocity.y < 0f)
+            waterCounter++;
+
+            //We should only apply effects to the player once they have entered the water
+            //It should not be applied multiple times with different water triggers
+            if (waterCounter == 1)
             {
-                dragVel.y = dragVel.y * (1 - Time.deltaTime * waterYAirDrag);
-                rb.velocity = transform.TransformDirection(dragVel);
+                Vector3 dragVel = transform.InverseTransformDirection(rb.velocity);
+                //Applying drag to y axis, only downward drag
+                //Applying downwards drag the moment the player enters the water
+                if (rb.velocity.y < 0f)
+                {
+                    dragVel.y = dragVel.y * (1 - Time.deltaTime * waterYAirDrag);
+                    rb.velocity = transform.TransformDirection(dragVel);
+                }
+
+                pm.inWater = true;
+                Debug.Log("We are in water");
+
+                pm.groundDrag = waterGroundDrag;
+                Physics.gravity = new Vector3(0, -waterGravity, 0);
             }
-
-
-            pm.inWater = true;
-            Debug.Log("We are in water");
-            //pm.walkSpeed = waterWalkSpeed;
-            //pm.sprintSpeed = waterSprintSpeed;
-            
-            pm.groundDrag = waterGroundDrag;
-            Physics.gravity = new Vector3(0,-waterGravity,0);
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
+        //We should only leave the water if we have left all water triggers
         if (other.tag == "water")
         {
-            pm.inWater = false;
+            waterCounter--;
+            if (waterCounter == 0)
+            {
+                pm.inWater = false;
+            }
             Debug.Log("We have left the water");
-            //pm.walkSpeed = walkSpeed;
-            //pm.sprintSpeed = sprintSpeed;
-
-            pm.airDrag = airDrag;
-            pm.groundDrag = groundDrag;
-            Physics.gravity = new Vector3(0, defaultGravity, 0);
         }
 
-        
+
     }
 
     private void FixedUpdate()
     {
-        if (pm.inWater && !pm.grounded) {
-            rb.AddForce(Vector3.down * waterDownwardForce, ForceMode.Force);
+        float sphereRadius = pm.playerHeight * 0.5f + 0.1f;
+        Vector3 origin = transform.position;
 
-            //rb.velocity = rb.velocity * (1 - Time.deltaTime * waterAirDrag);
+        // Check for water using OverlapSphere to detect trigger colliders
+        Collider[] hitColliders = Physics.OverlapSphere(origin, sphereRadius, whatIsWater);
+        bool inWater = false;
+
+        foreach (var collider in hitColliders)
+        {
+            if (collider.CompareTag("water"))
+            {
+                inWater = true;
+                break;
+            }
+        }
+
+        // If we are not in water but the player is in water, we should leave the water
+        //This will allow us to react to the water suddenly disappearing, due to our secret mechanic
+        //The water suddenly disappearing does not trigger OnTriggerExit
+        if (!inWater && pm.inWater)
+        {
+            LeaveWater();
+        }
+
+        if (pm.inWater && !pm.grounded)
+        {
+            rb.AddForce(Vector3.down * waterDownwardForce, ForceMode.Force);
 
             //Applying drag in all directions other than upwards
             //This will allow the player to feel like they are in water
             // whilst still being able to jump as high
-            Vector3 dragVel =  transform.InverseTransformDirection(rb.velocity);
+            Vector3 dragVel = transform.InverseTransformDirection(rb.velocity);
 
             //Applying a little drag to y axis
             if (rb.velocity.y < 0f)
@@ -115,5 +128,15 @@ public class Water : MonoBehaviour
             rb.velocity = transform.TransformDirection(dragVel);
             rb.angularDrag = waterXAirDrag;
         }
+    }
+
+
+    private void LeaveWater()
+    {
+        //Resetting the player's values to their default values
+        pm.inWater = false;
+        pm.airDrag = airDrag;
+        pm.groundDrag = groundDrag;
+        Physics.gravity = new Vector3(0, defaultGravity, 0);
     }
 }
